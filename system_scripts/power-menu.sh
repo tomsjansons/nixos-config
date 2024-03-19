e22a84362615e95c611a6f67cf4e8ea61f4872d0
Toms Jansons <toms.jansons@hood-software.lv>
2024-03-19 21:10:31 +0200

    hibernate

--- system_scripts/power-menu.sh
+++ system_scripts/power-menu.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n󱉚 Hibernate\n Suspend\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
                 ;&
         reboot)
                 systemctl $op
+                ;;
+        hibernate)
+                hyprlock & sleep 2 && systemctl $op
                 ;;
         suspend)
                 hyprlock & sleep 2 && systemctl $op
