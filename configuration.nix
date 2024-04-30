c77b4d0412cfc61202cc483d1bf980053a351173
Toms Jansons <toms.jansons@hood-software.lv>
2024-04-30 09:28:37 +0300

    zoom and zellij keybinds

--- configuration.nix
+++ configuration.nix
@@ -184,6 +184,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    zoom-us
     # yazi start
     ffmpegthumbnailer
     unar 
